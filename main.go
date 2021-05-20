ec99cc722a53bd882079d4f9c1a9d40f4c213e06
sunney-x <sanishubitidze@live.com>
2021-05-20 16:15:53 +0400

    removed unnecessary file exists check

--- main.go
+++ main.go
@@ -13,20 +13,13 @@ func main() {
 		panic("coverprofile path is required!")
 	}
 	fn := os.Args[1]
-	if _, err := os.Stat(fn); err != nil {
-		panic(fn + " does not exist")
-	}
-	profiles, err := getProfiles(fn)
+	profiles, err := cover.ParseProfiles(fn)
 	if err != nil {
 		panic(err)
 	}
 
 	fmt.Printf("Total coverage: %v\n", calculate(profiles))
 	os.Remove(fn)
-}
-
-func getProfiles(path string) (profiles []*cover.Profile, err error) {
-	return cover.ParseProfiles(path)
 }
 
 func calculate(profiles []*cover.Profile) string {
